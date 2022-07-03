dec0a8bff5bdde70ea65972a3fe7399a0ad94937
shivakumar323 <shivanavodaya007@gmail.com>
2022-07-03 14:18:31 +0530

    code to print subsequence in cadance algo

--- Arrays/problem11.js
+++ Arrays/problem11.js
@@ -25,15 +25,19 @@ The subarray is [-1]
 function max_subarray_sum(arr) {
     let sum = arr[0];
     let max_sum = arr[0];
-
+    let arr1 = [arr[0]];
     for(let i = 1; i < arr.length; i++) {
         if(sum < 0) {
             sum = arr[i];
+            arr1 = [];
+            arr1 = [arr[i]];
         } else {
             sum += arr[i];
+            arr1.push(arr[i]);
         }
         max_sum = Math.max(max_sum, sum);
     }
+    console.log(arr1);
     return max_sum;
 }
 
@@ -42,7 +46,8 @@ let arr = [1, 2, -5, 4, 3, 8, 5];
 console.log(max_subarray_sum(arr));
 
 /*
-output: 20
+output: [ 4, 3, 8, 5 ]
+20
 
 this algorithm is called kaden's algorithm
 
