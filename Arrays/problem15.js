dd2e8439f537cbfa0b12bd20cd9c7c10dcc74fb3
shivakumar323 <shivanavodaya007@gmail.com>
2022-07-23 12:08:52 +0530

    striver's sde sheet started

---
+++ Arrays/problem15.js
@@ -0,0 +1,19 @@
+// Finding majority element in an array(Moore's algorithm)
+
+/*
+Given an array of integers, find the majority element
+
+The majority element is the element that appears more than n/2 times
+where n is the size of an array
+
+For example: 
+
+Input: [3, 3, 2, 2, 3, 2, 3]
+Output: 3
+
+Input: [1, 1, 4, 1, 4, 4, 1, 4, 4]
+Ouput: 4
+
+
+
+*/
